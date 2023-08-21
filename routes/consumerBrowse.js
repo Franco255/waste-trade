34a972bfa8ace3a4a3d51ed29c923d0f82215db7
abra-abe <abewills17@gmail.com>
2023-08-21 13:02:56 +0300

    removed comments for cleaner code...

--- routes/consumerBrowse.js
+++ routes/consumerBrowse.js
@@ -29,7 +29,6 @@ router.get('/', (req, res) => {
             });
             const productDataJson = JSON.parse(JSON.stringify(productData));
 
-            // res.render('consumerBrowse.ejs', {productData});
             res.json(productDataJson);
         } finally {
             await client.close();
@@ -43,33 +42,5 @@ router.get('/', (req, res) => {
 router.get('/consumerBrowseView', (req, res) => {
     res.render('consumerBrowse.ejs');
 })
-
-// router.get('/consumerBrowseView', (req, res) => {
-//     //fetch from business db
-//     let uri = process.env.MONGO_URI;
-//     uri = uri.replace('<password>', process.env.MONGO_PASS);
-
-//     const client = new MongoClient(uri);
-
-//     async function getImages() {
-//         try{
-//             const db = client.db('test');
-//             const businesssellmodels = db.collection('businesssellmodels');
-    
-//             const productImages = await businesssellmodels.find({}).toArray();
-//             console.log(productImages);
-//             const imageArray = productImages.map(productImages => {
-//                 const imgDataBuffer = productImages[image].buffer;
-//                 const imgDataBase64 = imgDataBuffer.toString('base64');
-//                 return imgDataBase64
-//             })
-//             res.render('consumerBrowse.ejs', { imageArray });
-//         }finally {
-//             await client.close()
-//         }
-//     }
-    
-//     getImages();
-// })
 
 module.exports = router
